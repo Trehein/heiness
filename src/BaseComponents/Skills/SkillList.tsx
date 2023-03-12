6fd5844f822ef1cb1bd2d0765b2d617cff1676db
TreHein <treheinguy@gmail.com>
2023-03-12 08:45:45 -0500

    added static queries for collections and docs with id to SkillList

--- src/BaseComponents/Skills/SkillList.tsx
+++ src/BaseComponents/Skills/SkillList.tsx
@@ -1,28 +1,46 @@
 import React from 'react'
-import { useFirestoreQuery } from "@react-query-firebase/firestore";
+import { useFirestoreDocument, useFirestoreQuery } from "@react-query-firebase/firestore";
 import {
   query,
   collection,
+  QueryDocumentSnapshot,
+  DocumentData,
+  doc,
 } from "firebase/firestore";
 import { firestore } from '../../fbConfig/fbConfig.';
 
 const SkillList: React.FC = () => {
-  const ref = query(collection(firestore, "skills"));
-  const collectionQuery = useFirestoreQuery(["skills"], ref, {
-    subscribe: true,
-  });
-  const snapshot = collectionQuery.data
-  console.log('snapshot', snapshot?.docs[0])
+  // todo - use this for websocket queries
+  // const ref = query(collection(firestore, "skills"));
+  // const collectionQuery = useFirestoreQuery(["skills"], ref, {
+  //   subscribe: true,
+  // });
+  // const snapshot = collectionQuery.data
+  // console.log('snapshot', snapshot?.docs[0])
+
+  // static collection query
+  const queryRef = query(collection(firestore, 'skills'))
+  const collectionQuery = useFirestoreQuery(['skills'], queryRef)
+  const querySnapshot = collectionQuery.data
+
+  // static doc query
+  const docId = 'jGnrN4Wjr270nkGMb3s7'
+  const docRef = doc(firestore, 'skills', docId)
+  const skillFromId = useFirestoreDocument(['skills', docId], docRef)
+
+  const snapShotOfSkillFromId = skillFromId.data
+  console.log('skillSnapshot', snapShotOfSkillFromId?.data())
 
   return (
       <>
           { 
-              snapshot !== undefined &&
-              snapshot.docs.map((docSnapshot) => {
+              querySnapshot !== undefined &&
+              querySnapshot.docs.map((docSnapshot: QueryDocumentSnapshot<DocumentData>) => {
                   const data = docSnapshot.data();
                     console.log('data', data)
+                    console.log(docSnapshot.id)
                   return( 
-                    <div>
+                    <div key={docSnapshot.id}>
                         {data.title}
                     </div>
                 )
@@ -30,6 +48,7 @@ const SkillList: React.FC = () => {
           }
       </>
   )
+
 }
 
 export default SkillList
