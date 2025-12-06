56d2bd50f02e36d282ba71b3cc93c73f07ebb719
treheinguy <treheinguy@gmail.com>
2025-12-06 07:47:53 -0600

    added replayability widget

--- src/components/Widgets/ReviewWidgets/Replayability/ReplayabilityController.tsx
+++ src/components/Widgets/ReviewWidgets/Replayability/ReplayabilityController.tsx
@@ -1,9 +1,113 @@
 import React from 'react'
+import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend, Tooltip } from 'recharts'
+
+// new choices - companions
+// new actions / verbs
+// new growth - unlock
+// new challenges
+// new world - env, characters, interactions, explore
+
+// Radar Chart
+
+const replayabilityControllerStyles = () => {
+  return {
+    replayabilityInnerContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column' as 'column'
+    },
+    replayabilityHeaderTextContainer: {
+
+    },
+    replayabilityHeaderText: {
+      fontFamily: 'Righteous', 
+      fontSize: '1.5em',
+      margin: '0px',
+      padding: '0px',
+      borderTop: '1px solid black',
+      borderBottom: '1px solid black'
+    }
+  }
+}
+
+
+
+const mockReplayabilityData = [
+  {
+    replayFactor: 'Choices',
+    replayFactorRating: 2,
+    replayFactorDetailsText: 'some details about the choices you continue to make and will be able to change'
+  },
+    {
+    replayFactor: 'Actions',
+    replayFactorRating: 4,
+    replayFactorDetailsText: 'some details about the ACTIONS and how they are new verbs and stuff'
+  },
+    {
+    replayFactor: 'Growth',
+    replayFactorRating: 2,
+    replayFactorDetailsText: 'How does your character or your understanding of the games GROW'
+  },
+    {
+    replayFactor: 'Challenges',
+    replayFactorRating: 5,
+    replayFactorDetailsText: 'Are CHALLENGES worthwhile and meaningful tests of skill and understanding of systems or tedious grinds'
+  },
+    {
+    replayFactor: 'World',
+    replayFactorRating: 3,
+    replayFactorDetailsText: 'the WORLD continues to grow and change as you play'
+  }
+]
+
+// todo - overlay against GENRE and ALL
 
 const ReplayabilityController: React.FC = () => {
-  return (
-    <div>
+  console.log('replay', mockReplayabilityData)
+  const styles = replayabilityControllerStyles()
 
+  return (
+    <div className='replayabilityInnerContainer' style={styles.replayabilityInnerContainer}>
+      <h2 className='replayabilityHeaderText' style={styles.replayabilityHeaderText}>
+        Replayability
+      </h2>
+      <RadarChart style={{ width: '100%', maxWidth: '350px', maxHeight: '22.5vh', aspectRatio: 1 }} responsive data={mockReplayabilityData}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="replayFactor" fontSize={'.5em'}/>
+          <PolarRadiusAxis
+            tickCount={6}
+            domain={[0, 5]} // Set your desired domain here
+            tick={false} // Hide ticks
+            axisLine={false} // Hide axis line
+            tickFormatter={() => ''} // Hide tick labels
+          />          
+          {/* <Radar
+            name="Genre"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+            isAnimationActive={true}
+          /> */}
+          <Radar
+            name="This Game"
+            dataKey="replayFactorRating"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+            fillOpacity={0.6}
+            isAnimationActive={true}
+            dot={{ r: 3, fill: '#82ca9d' }}
+            activeDot={{ r: 6 }}
+          />
+          <Legend 
+            layout='horizontal' 
+            wrapperStyle={{fontSize: '.75em'}}
+            iconSize={7}
+            iconType='circle'
+          />
+          <Tooltip />
+        </RadarChart>
     </div>
   )
 }
--- src/screens/WidgetScreen.tsx
+++ src/screens/WidgetScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HighLowController from '../components/Widgets/ReviewWidgets/HighLow/HighLowController'
 import KindsOfFunController from '../components/Widgets/ReviewWidgets/KindsOfFun/KindsOfFunController'
+import ReplayabilityController from '../components/Widgets/ReviewWidgets/Replayability/ReplayabilityController'
 
 const WidgetScreen: React.FC = () => {
 
@@ -13,6 +14,9 @@ const WidgetScreen: React.FC = () => {
         </div>
         <div className='kindsOfFunContainer' style={{width: '25%', height: '30%', border: '1px solid green'}}>
           <KindsOfFunController />
+        </div>
+        <div className='replayabilityContainer' style={{width: '25%', height: '30%', border: '1px solid pink'}}>
+          <ReplayabilityController />
         </div>
       </div>
     </div>
